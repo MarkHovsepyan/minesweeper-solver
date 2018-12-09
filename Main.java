fa7b1336e40c54224a2972eb10a5345367faea25
unknown <garend95@yahoo.com>
2018-12-10 01:42:00 +0400

    generator does not print, otherwise no errors

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Your code here!
+
+        level test = new level(3,5,6);
+        test.getLevel();
+    }
+}
